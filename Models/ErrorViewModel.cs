e83c2a59efffee547a912f0688ac23ac6bff08e6
Melissa Turbano <melissaturbano@gmail.com>
2022-09-16 22:14:26 -0300

    estrutura base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AvaliacaoLp3_Shopping.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
